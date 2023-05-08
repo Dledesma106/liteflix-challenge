e7d4cc9e7436743cd632ac73b7581e28b8b4b3de
Dledes <davidpewen_101@hotmail.com>
2023-05-08 00:07:06 -0300

    fixed broken interface

--- components/interfaces.ts
+++ components/interfaces.ts
@@ -1,6 +1,8 @@
 import { type HighlightedMovie, type Movie } from 'lib/moviesApi'
+import type { MyMovie } from 'backend/models/MyMovie'
 
 export interface MoviesProps {
 	highlightedMovie: HighlightedMovie
 	popularMovies: Movie[]
+	myMovies: MyMovie[]
 }
